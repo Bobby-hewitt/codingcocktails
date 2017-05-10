ee905fe5ce677867f50a49cbe341c1ae99e5ede7
Bobby-hewitt <bobbyhewitt@hotmail.co.uk>
2017-05-10 21:59:40 +0100

    first coomit

--- server/app.js
+++ server/app.js
@@ -4,9 +4,8 @@ const path = require('path');
 const app = express();
 
 // Serve static assets
-app.use(express.static('../build'));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-// Always return the main index.html, so react-router render the route in the client
 
 
 module.exports = app;
