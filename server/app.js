f2e955128bd5a311d223bb23dd7224151e0822bc
Bobby-hewitt <bobbyhewitt@hotmail.co.uk>
2017-05-10 22:48:20 +0100

    first coomit

--- server/app.js
+++ server/app.js
@@ -8,5 +8,8 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 
 module.exports = app;
